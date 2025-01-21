4b3889ddfbba0b3257f58e9ba2fcd3119787c27e
Sheeeple <sheeple359@gmail.com>
2025-01-21 16:48:44 -0500

    i think i fixed it

--- src/main/java/satisfish/com/item/ModItems.java
+++ src/main/java/satisfish/com/item/ModItems.java
@@ -4,20 +4,25 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import satisfish.com.Satisfsh;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+
 public class ModItems {
+
+    // Registering the UWU item
     public static final Item UWU = registerItem("uwu", new Item(new Item.Settings()));
 
-
-    private static  Item registerItem(String name, Item item) {
+    // Helper method to register an item
+    private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Satisfsh.MOD_ID, name), item);
     }
 
+    // Method to register the items in the ingredients group
     public static void registerModItems() {
         Satisfsh.LOGGER.info("Registering Mod Items for " + Satisfsh.MOD_ID);
 
+        // Adding the UWU item to the INGREDIENTS item group
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(UWU);
         });
