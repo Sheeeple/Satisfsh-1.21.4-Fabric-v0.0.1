deb30d99a25c0179f9e60b59ba8bf6f336c86592
Sheeeple <sheeple359@gmail.com>
2025-01-21 14:41:34 -0500

    Initial commit

---
+++ src/main/java/satisfish/com/Satisfsh.java
@@ -0,0 +1,16 @@
+package satisfish.com;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Satisfsh implements ModInitializer {
+	public static final String MOD_ID = "satisfsh";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
